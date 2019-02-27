ba09b7ac18915af8b6ac3feef10bf3ee3e9d21ea
Thomas Manson <dev.mansonthomas@gmail.com>
2019-02-27 22:34:45 +0100

    * add extra file for the sample

--- findQueteurById2/index.js
+++ findQueteurById2/index.js
@@ -61,18 +61,21 @@ exports.addMessage = functions.https.onCall((data, context) => {
   const text = data.text;
   // [END readMessageData]
   // [START messageHttpsErrors]
-  // Checking attribute.
-  if (!(typeof text === 'string') || text.length === 0) {
-    // Throwing an HttpsError so that the client gets the error details.
-    throw new functions.https.HttpsError('invalid-argument', 'The function must be called with ' +
-      'one arguments "text" containing the message text to add.');
-  }
+
   // Checking that the user is authenticated.
   if (!context.auth) {
     // Throwing an HttpsError so that the client gets the error details.
     throw new functions.https.HttpsError('failed-precondition', 'The function must be called ' +
       'while authenticated.');
   }
+
+  // Checking attribute.
+  if (!(typeof text === 'string') || text.length === 0) {
+    // Throwing an HttpsError so that the client gets the error details.
+    throw new functions.https.HttpsError('invalid-argument', 'The function must be called with ' +
+      'one arguments "text" containing the message text to add.');
+  }
+
   // [END messageHttpsErrors]
 
   // [START authIntegration]
