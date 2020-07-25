03f82a865ab85045440516da0d5cf4f2858ecdfb
Thomas Manson <dev.mansonthomas@gmail.com>
2020-07-25 16:19:31 +0200

    Commit before deployment

--- RCQ/ULTriggerRecompute/common.js
+++ RCQ/ULTriggerRecompute/common.js
@@ -66,7 +66,7 @@ async function initMySQL(secretName) {
 
 async function getSecret(secretName){
   // Access the secret.
-  let secretPath = "projects/"+project+"-"+country+"-"+env+"/secrets/"+secretName+"/versions/latest";
+  let secretPath = "projects/"+project.toLowerCase()+"-"+country+"-"+env+"/secrets/"+secretName+"/versions/latest";
   console.trace("accessing secret with path "+secretPath);
   const [accessResponse] = await secretManagerServiceClient.accessSecretVersion({name: secretPath});
   return accessResponse.payload.data.toString('utf8');
