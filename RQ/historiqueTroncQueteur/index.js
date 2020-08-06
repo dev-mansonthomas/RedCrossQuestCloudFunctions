13b75517b0b116e26ee2b1abe47b31ef81f5558d
Thomas Manson <dev.mansonthomas@gmail.com>
2020-08-06 22:05:54 +0200

    Commit before deployment

--- RQ/historiqueTroncQueteur/index.js
+++ RQ/historiqueTroncQueteur/index.js
@@ -2,6 +2,7 @@
 const common              = require('./common');
 const common_firestore    = require('./common_firestore');
 const common_firebase     = require('./common_firebase' );
+const common_mysql        = require('./common_mysql');
 
 const moment    = require('moment');
 
@@ -86,7 +87,7 @@ const queryStr = `
 exports.historiqueTroncQueteur = functions.https.onCall(async (data, context) => {
 
   common_firebase.checkAuthentication(context);
-  let mysqlPool = await common.initMySQL('MYSQL_USER_READ');
+  let mysqlPool = await common_mysql.initMySQL('MYSQL_USER_READ');
 
   const uid     = context.auth.uid;
   const name    = context.auth.token.name    || null;
